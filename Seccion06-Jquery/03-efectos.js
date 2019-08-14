c42b73e01ac694fed61ce5a884fdb3bf731ae148
rodricerdan09 <cerdanrodrigo.utn@gmail.com>
2019-08-14 02:02:15 -0300

    Added finished Jquery section

---
+++ Seccion06-Jquery/03-efectos.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+
+	/*
+		Nuestro boton no puede acceder a la funcion efecto
+		porque esta protegida por la funcion de la primera linea.
+		Si la comentamos ya tendra acceso.
+	*/
+
+	function efecto(){
+		// $(selector).efecto(velocidad, callback);
+
+
+		// $('.caja').hide(3000, function(){
+		// 	alert('Oculto');
+		// });
+
+		// $('.caja').show();
+		// $('.caja').toggle();
+		
+
+		// Fades - Desvanecerse
+		// $('.caja').fadeIn();
+		// $('.caja').fadeOut();
+		// $('.caja').fadeToggle();
+
+		// Sliding - Deslizamiento
+		// $('.caja').slideDown();
+		// $('.caja').slideUp();
+		// $('.caja').slideToggle();
+
+
+	}
+// });
---
+++ Seccion06-Jquery/04-dom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+// Parent
+	// $('#tercera').parent().css({
+	// 	background: '#1b3d82'
+	// });
+
+// Parents
+	// $('#tercera').parents();
+
+// Children
+	// $('#padre').children().fadeOut(500);
+	// $('#padre').children('#tercera').fadeOut(1000);
+
+// Find
+	// $('#contenedor').find('div.caja').slideUp();
+
+// Siblings
+	// $('#tercera').siblings().fadeOut(1500);
+
+// Next - Prev
+	/* $('#tercera').nextAll().css({
+		background: '#000' }); */
+
+	$('#tercera').prevAll().css({
+		background: '#000'
+	});
+
+});
